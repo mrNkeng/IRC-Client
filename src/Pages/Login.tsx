db9efcc7f68b3e7d15771fbdd555d6188d9a5b8e
ksomerville31 <ksomerville31@outlook.com>
2022-10-24 23:48:30 -0400

    added super basic sign in page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React from "react";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import flexbox from "@mui/system";
+import Button from "@mui/material/Button";
 
-interface LoginProps {
-}
+interface LoginProps {}
 
 export const Login = (props: LoginProps) => {
   return (
-    <body>
-      <h1>LOGIN PAGE</h1>
-    </body>
-
+    //we can style this correctly later :P
+    <Box
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <InputLabel>Sign In</InputLabel>
+      <FormControl>
+        <body />
+        <TextField label="Email Address" />
+        <body /> {/*idk how else to add space here */}
+        <TextField label="Password" />
+        <body />
+        <Button
+          onClick={() => {
+            alert("you're signed in");
+          }}
+          size="small"
+          variant="outlined"
+        >
+          Sign In
+        </Button>
+      </FormControl>
+    </Box>
   );
 };
-
