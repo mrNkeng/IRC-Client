eeba45fa2ec85a8e49e6e04aff5eca00f39b0c9b
Jedan Davis <jedandavis@jedans-mbp.mynetworksettings.com>
2022-11-22 21:59:34 -0500

    hhsjshshsh

--- src/renderer/Pages/Home.tsx
+++ src/renderer/Pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
-import { Grid, Button, Box, Typography, Tooltip, IconButton } from "@mui/material";
+import { Grid, Button, Box, Typography, Tooltip, IconButton, styled, Paper, MenuList, MenuItem, ListItemIcon, ListItemText, Divider, makeStyles, Theme } from "@mui/material";
 import { visitEachChild } from "typescript";
 import { ChatWindow } from "renderer/components/Chat/ChatWindow";
 import { color } from "@mui/system";
+import MessageIcon from '@mui/icons-material/Message';
+import BlockIcon from '@mui/icons-material/Block';
 
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import { Cloud, ContentCopy, ContentCut, ContentPaste } from "@mui/icons-material";
 
 interface HomeProps { }
 
@@ -17,6 +20,7 @@ interface User {
   userName: string;
 
 }
+
 
 const servers: ReadonlyArray<Server> = [
   { serverName: "Coolest Server Ever" },
@@ -47,6 +51,7 @@ const users: ReadonlyArray<User> = [
   { userName: "Ryan" },
 ];
 
+
 interface ServerListProps {
   servers: ReadonlyArray<Server>;
   setServer: (server: Server) => void;
@@ -59,15 +64,15 @@ interface UserListProps {
 const ServerList = (props: ServerListProps) => {
   return (
     <Box style={{ margin: "0px", padding: "0px" }}>
-      <Typography sx={{fontSize: 'large', 
-            textAlign: 'center', 
+      <Typography sx={{fontSize: 'large',
+            textAlign: 'center',
             paddingTop:' 10px',
             paddingBottom: '10px',
             fontWeight: 'bold',
             color: 'lightgrey' }}>Server List</Typography>
       <Stack>
-        {props.servers.map((server) => (
-          <Tooltip title= {server.serverName} placement="right" arrow >
+        {props.servers.map((server, index) => (
+          <Tooltip title= {server.serverName} placement="right" arrow key={index}>
          <IconButton  size="large" color= "secondary" onClick={() => props.setServer(server)}>
          <CatchingPokemonIcon fontSize="inherit" />
          </IconButton>
@@ -79,21 +84,77 @@ const ServerList = (props: ServerListProps) => {
 };
 
 const UserList = (props: UserListProps) => {
+  const [show, setShow] = useState(false);
+  const [points, setPoints] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleClick = () => setShow(false);
+    window.addEventListener('click', handleClick);
+    return () => window.removeEventListener('click', handleClick);
+  }, []);
+
+  const boxWidth = 180;
+  // TODO: remove any and get react event type
+  const onContextMenu = (event: any) => {
+      event.preventDefault();
+      setShow(true);
+      let x = event.pageX;
+      let y = event.pageY;
+      //
+      if (window.screen.width <= boxWidth + x) {
+        x = window.screen.width - boxWidth;
+      }
+      setPoints({ x: x, y: y });
+  }
+
+  const onMessage = () => {
+    console.log("sending message")
+  }
+
+  const onBlock = () => {
+
+  }
+
+  const style = { width: boxWidth, maxWidth: '50%', borderRadius: '5px', top: points.y, left: points.x, position: "absolute"}
+
   return (
     <Box style={{ margin: "0px", padding: "0px" }}>
-      <Typography sx={{fontSize: 'large', 
-            textAlign: 'center', 
+      <Typography sx={{fontSize: 'large',
+            textAlign: 'center',
             paddingTop:' 10px',
             fontWeight: 'bold',
             color: '#2E8B57' }} >Online</Typography>
-      <Stack sx={{fontSize: 'large', 
-            textAlign: 'center', 
+      <Stack sx={{fontSize: 'large',
+            textAlign: 'center',
             paddingTop:' 10px',
             fontWeight: 'bold',
             color: 'lightgrey' }} >
-        {props.users.map((user) => (
-          <Typography>{user.userName}</Typography>
+        {props.users.map((user, index) => (
+          <Typography key={index} onContextMenu={onContextMenu}>{user.userName}</Typography>
         ))}
+        {show && (
+        <Paper sx={style}>
+        <MenuList sx= {{ backgroundColor: '#383838', borderRadius: '5px', position:'absolute'}}>
+          <MenuItem sx= {{ backgroundColor: '#7289da'}} onClick={onMessage}>
+            <ListItemText>Message</ListItemText>
+            <ListItemIcon >
+              <MessageIcon fontSize="small" />
+            </ListItemIcon>
+          </MenuItem>
+
+          <Divider />
+
+          <MenuItem sx= {{ backgroundColor: '#7289da'}} onClick={onBlock}>
+            <ListItemText>Block</ListItemText>
+            <ListItemIcon>
+              <BlockIcon fontSize="small"/>
+            </ListItemIcon>
+          </MenuItem>
+
+
+        </MenuList>
+      </Paper>
+      )}
       </Stack>
     </Box>
   );
@@ -116,9 +177,9 @@ export const Home = (props: HomeProps) => {
             borderBottom: '2px solid #2C2F33'
         }}>
           <span className="heading">
-            <Box sx = 
-            {{fontSize: 'large', 
-            textAlign: 'center', 
+            <Box sx =
+            {{fontSize: 'large',
+            textAlign: 'center',
             paddingTop:' 10px',
             fontWeight: 'bold',
             color: 'lightgrey' }}>AOL Messenger</Box>
@@ -145,9 +206,9 @@ export const Home = (props: HomeProps) => {
           style={{
             height: "100vh",
             backgroundColor: "#282b30",
-        }}>  
-      <Typography sx={{fontSize: 'large', 
-            textAlign: 'center', 
+        }}>
+      <Typography sx={{fontSize: 'large',
+            textAlign: 'center',
             paddingTop:' 10px',
             paddingBottom: '10px',
             fontWeight: 'bold',
